7251a01bd02ddc53aaf7b69eae15f9eea7d5288c
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-11-06 21:16:45 -0300

    Update conta.py
    Getters and setters of Dono, agencia, tipo, extrato and saldo

--- conta.py
+++ conta.py
@@ -1,4 +1,49 @@
 
 
 class Conta:
-    pass
+    def __init__(self, codigo_dono, codigo_agencia, tipo, extrato, saldo = 0):
+        self.__codigo_dono = codigo_dono
+        self.__codigo_agencia = codigo_agencia
+        self.__tipo = tipo
+        self.__extrato = extrato
+        self.__saldo = saldo
+
+    @property
+    def codigo_dono(self):
+        return self.__codigo_dono
+
+    @codigo_dono.setter
+    def codigo_dono(self, novo_codigo_dono):
+        self.__codigo_dono = novo_codigo_dono
+
+    @property
+    def codigo_agencia(self):
+        return self.__codigo_agencia
+
+    @codigo_agencia.setter
+    def codigo_agencia(self, novo_codigo_agencia):
+        self.__codigo_agencia = novo_codigo_agencia
+
+    @property
+    def tipo(self):
+        return self.__tipo
+
+    @tipo.setter
+    def tipo(self, novo_tipo):
+        self.__tipo = novo_tipo
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @saldo.setter
+    def saldo(self, novo_saldo):
+        self.__saldo = novo_saldo
+
+    @property
+    def extrato(self):
+        return self.__extrato
+
+    @extrato.setter
+    def extrato(self, novo_extrato):
+        self.__extrato = novo_extrato
