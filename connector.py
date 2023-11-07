6c5007aacf1cea4bed9989d16664da0b2652b851
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-11-07 20:19:23 -0300

    Update connector.py
    Update and delete

--- connector.py
+++ connector.py
@@ -15,7 +15,7 @@ class Connector:
 
     def criar(self, tipo, *args):
         match tipo:
-            case "Agencia":
+            case 'Agencia':
                 pass
             case 'Cliente':
                 pass
@@ -24,18 +24,116 @@ class Connector:
                 with open(self.path_bd) as bd_json:
                     data = json.load(bd_json)   #transformo json em dicionario
                     coluna_conta = data["BD"][tipo] #escolho a coluna
-                    codigo_ultimo = coluna_conta[-1]["Codigo"]  #pego o codigo da ultima conta
-                    coluna_conta.append({"Codigo": codigo_ultimo + 1, "Codigo_dono": args[1], "Codigo_agencia": args[2], "Tipo": args[3], "Extrato": args[4], "Saldo": args[5]})    #salvo a conta no ultimo lugar da coluna
+                    try:
+                        codigo_ultimo = coluna_conta[-1]["Codigo"]  #pego o codigo da ultima conta
+                    except IndexError:
+                        codigo_ultimo = 0
+                    coluna_conta.append({"Codigo": codigo_ultimo + 1, "Codigo_dono": args[0], "Codigo_agencia": args[1], "Tipo": args[2], "Extrato": args[3], "Saldo": args[4]})    #salvo a conta no ultimo lugar da coluna
                 with open(self.path_bd, 'w') as bd_json:
                     json.dump(data, bd_json)    #salvo as alteracoes no bd.json
+                return True
 
             case 'Movimento':
                 with open(self.path_bd) as bd_json:
                     data = json.load(bd_json)   #transformo json em dicionario
                     coluna_movimento = data["BD"][tipo] #escolho a coluna
-                    codigo_ultimo = coluna_movimento[-1]["Codigo"]  #pego o codigo da ultima conta
-                    coluna_movimento.append({"Codigo": codigo_ultimo + 1, "Codigo_conta_inicial": args[1], "Codigo_conta_final": args[2], "Saldo_anterior": args[3], "Saldo_posterior": args[4], "codigo_movimento_anterior": args[5], "is_Saida": args[6]})    #salvo a conta no ultimo lugar da coluna
+                    try:
+                        codigo_ultimo = coluna_movimento[-1]["Codigo"]  #pego o codigo da ultima conta
+                    except IndexError:
+                        codigo_ultimo = 0
+                    coluna_movimento.append({"Codigo": codigo_ultimo + 1, "Codigo_conta_inicial": args[0], "Codigo_conta_final": args[1], "Saldo_anterior": args[2], "Saldo_posterior": args[3], "codigo_movimento_anterior": args[4], "is_Saida": args[5]})    #salvo a conta no ultimo lugar da coluna
                 with open(self.path_bd, 'w') as bd_json:
                     json.dump(data, bd_json)    #salvo as alteracoes no bd.json
+                return True
+            case _:
+                return None
+            
+    def atualizar(self, tipo, codigo, **kwargs):
+        match tipo:
+            case 'Agencia':
+                pass
+            case 'Cliente':
+                pass
+            case 'Conta':
+                certo = False
+                with open(self.path_bd) as bd_json:
+                    data = json.load(bd_json)
+                    coluna_conta = data["BD"][tipo] #escolho a coluna
+                    lugar = -1
+                    for index, conta in enumerate(coluna_conta):
+                        if conta["Codigo"] == codigo:
+                            lugar = index
+                    if lugar != -1:
+                        for key, value in enumerate(kwargs.items()):
+                            conta[lugar][key] = value
+                        certo = True
+                    else:
+                        print("Objeto nao encontrado!")
+                with open(self.path_bd, 'w') as bd_json:
+                    json.dump(data, bd_json)
+                return certo
+
+            case 'Movimento':
+                certo = False
+                with open(self.path_bd) as bd_json:
+                    data = json.load(bd_json)
+                    coluna_movimento = data["BD"][tipo] #escolho a coluna
+                    lugar = -1
+                    for index, movimento in enumerate(coluna_movimento):
+                        if movimento["Codigo"] == codigo:
+                            lugar = index
+                    if lugar != -1:
+                        for key, value in enumerate(kwargs.items()):
+                            movimento[lugar][key] = value
+                        certo = True
+                    else:
+                        print("Objeto nao encontrado!")
+                with open(self.path_bd, 'w') as bd_json:
+                    json.dump(data, bd_json)
+                return certo
+            case _:
+                return None
+            
+    def deletar(self, tipo, codigo):
+        match tipo:
+            case 'Agencia':
+                pass
+            case 'Cliente':
+                pass
+            case 'Conta':
+                certo = False
+                with open(self.path_bd) as bd_json:
+                    data = json.load(bd_json)
+                    coluna_conta = data["BD"][tipo] #escolho a coluna
+                    lugar = -1
+                    for index, conta in enumerate(coluna_conta):
+                        if conta["Codigo"] == codigo:
+                            lugar = index
+                    if lugar != -1:
+                        coluna_conta.pop(lugar)
+                        certo = True
+                    else:
+                        print("Objeto nao encontrado!")
+                with open(self.path_bd, 'w') as bd_json:
+                    json.dump(data, bd_json)
+                return certo
+
+            case 'Movimento':
+                certo = False
+                with open(self.path_bd) as bd_json:
+                    data = json.load(bd_json)
+                    coluna_movimento = data["BD"][tipo] #escolho a coluna
+                    lugar = -1
+                    for index, movimento in enumerate(coluna_movimento):
+                        if movimento["Codigo"] == codigo:
+                            lugar = index
+                    if lugar != -1:
+                        coluna_movimento.pop(lugar)
+                        certo = True
+                    else:
+                        print("Objeto nao encontrado!")
+                with open(self.path_bd, 'w') as bd_json:
+                    json.dump(data, bd_json)
+                return certo
             case _:
                 return None
