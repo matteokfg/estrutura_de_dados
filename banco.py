5684eaeddd99c50812ab0d8a5d147da290e8226c
matteokfg <67129306+matteokfg@users.noreply.github.com>
2023-11-08 16:18:22 -0300

    feat: class Banco

--- banco.py
+++ banco.py
@@ -1,2 +1,24 @@
-class Banco:
-    pass
+from connector import Connector
+
+
+class Banco(Connector):
+    def __init__(self, codigo, nome):
+        self.__codigo = codigo
+        self.__nome = nome
+        Connector.__init__('bd.json')
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @codigo.setter
+    def codigo(self, novo_codigo):
+        self.__codigo = novo_codigo
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome
